73d45bcd9211c66eaca49a5fa340da2f01bec2e9
Yan Yi Goh <yygoh93@gmail.com>
2016-06-25 14:22:40 +0800

    Application Controller: Add authenticate method
    Added authentication method that can be called in all children
    controllers that will need to authenticate the token being sent in for
    specific user actions.
    
    Also added RecordNotFound 404 JSON message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+
+  # When database record is not found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  def not_found
+    render json: { error: 'Not found' }, status: 404
+  end
+
+  private
+
+  def authenticate_user_from_token!
+    user_email = request.headers['HO-Email']
+    user       = user_email && User.find_by_email(user_email)
+    user_token = request.headers['HO-Token']
+    token      = user_token && User.find_by_token(user_token)
+
+    if user && token
+      @user = User.find_by_email(user_email)
+    else
+      render json: { error: 'Unable to authenticate user. Please check the request headers.' }, status: 401
+    end
+  end
 end
