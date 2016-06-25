76db5b295b22e78a4334751ec01666052c0eee6a
Yan Yi Goh <yygoh93@gmail.com>
2016-06-25 17:42:12 +0800

    Seed: Add creating of database items via seed
    Run rake db:seed to populate the database with the items. This file has
    been modified to create:
    - Activities
    - Areas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create areas
+Area.create(:name => "Ang Mo Kio")
+Area.create(:name => "Choa Chu Kang")
+Area.create(:name => "Jurong East")
+Area.create(:name => "Pasir Ris")
+Area.create(:name => "Sengkang")
+Area.create(:name => "Serangoon")
+Area.create(:name => "Tampines")
+Area.create(:name => "Tiong Bahru")
+Area.create(:name => "Toa Payoh")
+
+# Create activities
+Activity.create(:name => "Badminton")
+Activity.create(:name => "Basketball")
+Activity.create(:name => "BBQ")
+Activity.create(:name => "Bird Watching")
+Activity.create(:name => "Kids Activity")
+Activity.create(:name => "Physical Training")
+Activity.create(:name => "Soccer")
+Activity.create(:name => "Table Tennis")
